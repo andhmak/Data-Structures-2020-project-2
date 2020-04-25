4a9f6bc2bdfede369f2d3b4136413c6e74f6e184
andhmak <sdi1900047@di.uoa.gr>
2020-04-25 19:35:20 +0300

    doc

--- programs/vector_insert_steps/vector_insert_steps.c
+++ programs/vector_insert_steps/vector_insert_steps.c
@@ -6,6 +6,7 @@
 #define N 10000
 
 int main(int argc, char **argv) {
+    // mode == 0 αν real, 1 αν amortized
     char mode;
     if (argc != 2) {
         fprintf(stderr, "Invalid number of arguments\n");
@@ -21,10 +22,14 @@ int main(int argc, char **argv) {
         fprintf(stderr, "Expected \"real\" or \"amortized\" as argument\n");
         return 2;
     }
+    // total ο συνολικός αριθμός βημάτων
     int total = 0;
     Vector vec = vector_create(0, NULL);
     vector_insert_last(vec, NULL);
     total += vector_steps(vec);
+    // τα amortized/real έχουν μια "απόσταση" ενός βήματος, αφού το n έχει διαφορετική σημασία για το καθένα
+    // (η amortized μετράει τον μέσο όρο μέχρι και n-οστό στοιχείο, ενώ η real μετράει τα βήματα αν υπάρχουν ήδη n στοιχεια)
+    // άρα η amortized θα εκτελέσει μόνη της το πρώτο βήμα και η real μόνη της το τελευταίο, από συνολικά Ν + 1 βήματα
     if (mode) {
         printf("%d,%d\n", 1, total);
     }
